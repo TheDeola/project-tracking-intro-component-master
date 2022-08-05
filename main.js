79dbd63cff0ab48277e249a9559cd29d9fca1d03
TheDeola <aadetunji012@gmail.com>
2022-08-05 10:21:01 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+function myFunction() {
+    var hamburger = document.getElementById("myLinks");
+    if (hamburger.style.display === "block") {
+      hamburger.style.display = "none";
+    } else {
+      hamburger.style.display = "block";
+    }
+}
